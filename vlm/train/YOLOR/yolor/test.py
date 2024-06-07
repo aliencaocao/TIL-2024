c736aaf9e55c1a1cea4303d79eb91176acc22d7f
Ho Wing Yip <sploooosh884@gmail.com>
2024-06-06 21:14:34 -0800

    Add YOLOR finetune run + delete YOLOR-P6 test runs

--- vlm/train/YOLOR/yolor/test.py
+++ vlm/train/YOLOR/yolor/test.py
@@ -25,7 +25,7 @@ def test(data,
          imgsz=640,
          conf_thres=0.001,
          iou_thres=0.6,  # for NMS
-         save_json=True,
+         save_json=False,
          single_cls=False,
          augment=False,
          verbose=False,
@@ -242,7 +242,7 @@ def test(data,
     # Save JSON
     if save_json and len(jdict):
         w = Path(weights[0] if isinstance(weights, list) else weights).stem if weights is not None else ''  # weights
-        anno_json = glob.glob('../data/custom/annotations/instances_val*.json')[0]  # annotations json
+        anno_json = glob.glob('data/custom/annotations/instances_val*.json')[0]  # annotations json
         pred_json = str(save_dir / f"{w}_predictions.json")  # predictions json
         print('\nEvaluating pycocotools mAP... saving %s...' % pred_json)
         with open(pred_json, 'w') as f:
